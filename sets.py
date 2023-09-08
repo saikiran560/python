8a7d196d1f61db583dd49c0d4b19bcf71ea7acf9
saikiran560 <82799459+saikiran560@users.noreply.github.com>
2023-09-08 10:29:56 +0530

    Add files via upload

--- sets.py
+++ sets.py
@@ -3,3 +3,32 @@ thisset = {"apple", "banana", "cherry", "apple"}
 print(thisset)
 print(len(thisset))
 print(type(thisset))
+for x in thisset:
+ print(x)
+print("banana" in thisset)
+thisset.add("orange")
+
+print(thisset)
+
+tropical = {"pineapple", "mango", "papaya"}
+thisset.update(tropical)
+
+print(thisset)
+
+mylist = ["kiwi", "orange"]
+thisset.update(mylist)
+print(thisset)
+
+
+thisset.remove("banana")
+print(thisset)
+
+thisset.pop()
+print(thisset)
+
+#thisset.clear()
+#print(thisset)
+
+#del thisset
+#print(thisset)
+
