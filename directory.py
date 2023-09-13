322d2e8b582af03f993ac49eba04b88ee2acd946
saikiran560 <82799459+saikiran560@users.noreply.github.com>
2023-09-13 11:02:41 +0530

    Add files via upload

---
+++ directory.py
@@ -0,0 +1,9 @@
+import os
+# os.mkdir("d:\\FilesHandlingDemo")
+# print("the files list in current directory",os.listdir("d:\\FilesHandlingDemo"))
+# os.remove("d:\\FilesHandlingDemo\\New Microsoft Excel Worksheet.xlsx")
+print("the files list in current directory",os.listdir("d:\\FilesHandlingDemo"))
+# os.remove("d:\\FilesHandlingDemo\\New Text Document.txt")
+# print("the files list in current directory",os.listdir("d:\\FilesHandlingDemo"))
+os.removedirs("d:\\FilesHandlingDemo")
+
