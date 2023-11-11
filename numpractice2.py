d0618cad3dee5712dbbb669054a73358400bd7f6
saikiran560 <82799459+saikiran560@users.noreply.github.com>
2023-11-11 12:03:18 +0530

    Add files via upload

---
+++ numpractice2.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# Creating a 1D array
+arr_1d = np.array([1, 2, 3])
+print("1D Array:")
+print(arr_1d)
+print("Number of Dimensions (ndim):", arr_1d.ndim)
+print("Shape:", arr_1d.shape)
+
+
+
+
+# Creating a 2D array
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print("2D Array:")
+print(arr_2d)
+print("Number of Dimensions (ndim):", arr_2d.ndim)
+print("Shape:", arr_2d.shape)
+
+
+
+
+# Creating a 3D array
+arr_3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print("3D Array:")
+print(arr_3d)
+print("Number of Dimensions (ndim):", arr_3d.ndim)
+print("Shape:", arr_3d.shape)
